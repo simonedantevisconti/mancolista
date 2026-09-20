dd2e12ed72f8b4b3849f021db11b4977becc47ad
Simone Dante Visconti <136102134+simonedantevisconti@users.noreply.github.com>
2026-09-21 01:11:07 +0200

    Show mobile install helper in layout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import InstallApp from "../components/InstallApp";
 
 const DefaultLayout = () => {
   const location = useLocation();
@@ -16,6 +17,7 @@ const DefaultLayout = () => {
       </main>
 
       <Footer />
+      <InstallApp />
     </div>
   );
 };

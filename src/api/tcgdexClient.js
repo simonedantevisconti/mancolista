06b418f9df3b6752e7077c3112ec8f3c94a61daf
Simone Dante Visconti <136102134+simonedantevisconti@users.noreply.github.com>
2026-09-23 15:30:43 +0200

    Remove TCGdex client

--- src/api/tcgdexClient.js
+++
@@ -1,7 +0,0 @@
-import TCGdex from "@tcgdex/sdk";
-
-export const tcgdexIt = new TCGdex("it");
-export const tcgdexEn = new TCGdex("en");
-
-// Compatibilità con il codice già esistente
-export const tcgdex = tcgdexIt;

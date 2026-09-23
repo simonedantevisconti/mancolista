476e163dfce3dad9dc8ff1721c997d5ea2a6c4d0
Simone Dante Visconti <136102134+simonedantevisconti@users.noreply.github.com>
2026-09-23 15:29:58 +0200

    Remove Pokemon provider

--- src/data/collectionProviders.js
+++ src/data/collectionProviders.js
@@ -1,10 +1,1 @@
-import { getPokemonSetById, getPokemonSets } from "../services/pokemonService";
-
-export const collectionProviders = {
-  pokemon: {
-    id: "pokemon",
-    label: "Pokémon TCG",
-    getSets: getPokemonSets,
-    getSetById: getPokemonSetById,
-  },
-};
+export const collectionProviders = {};
